0fbfcb35357cce332c7a23a078dc268a45c4c98f
Tammer Kamel <tammer@ilukacg.com>
2013-12-01 10:54:47 -0500

    init commit

---
+++ shibor.rb
@@ -0,0 +1,29 @@
+require "nokogiri"
+require 'open-uri'
+
+
+#
+# Shibor, last 10 days
+#
+
+puts <<META
+code: SHIBOR
+name: Shibor
+description: Shaghai Interbank Offer Rate
+display_url: http://www.shibor.org/shibor/web/AllShibor_e.jsp
+--
+META
+
+doc = Nokogiri::HTML(open('http://www.shibor.org/shibor/ShiborTendaysShow_e.do'))
+
+doc.css('#result tr').each do |row|
+	row = row.css('td')
+	print row.shift.content
+	row.each do |item|
+		print ", #{item.content}"
+	end
+	puts
+end
+
+
+
