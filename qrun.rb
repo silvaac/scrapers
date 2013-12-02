adb2eefa7cf928bd9ea6911b89c77f40f99edd9f
Tammer Kamel <tammer@ilukacg.com>
2013-12-02 17:27:11 -0500

    get the output into the email

--- qrun.rb
+++ qrun.rb
@@ -7,20 +7,20 @@ def sendMail (body)
 end
 
 # Run the scraper:
-stdout,stderr,status = Open3.capture3("ruby #{ARGV[0]}")
+stdout1,stderr1,status = Open3.capture3("ruby #{ARGV[0]}")
 
-unless stderr == ''
-  puts stderr
-  sendMail(stderr)
+unless stderr1 == ''
+  puts stderr1
+  sendMail(stderr1)
   exit
 end
 
 puts "Scraper sent nothing to stderr. so now piping stdout to quandl upload"
 
-puts stdout
+puts stdout1
 
-stdout, stderr, status = Open3.capture3("quandl upload", :stdin_data => stdout)
+stdout2, stderr2, status = Open3.capture3("quandl upload", :stdin_data => stdout1)
 
-puts stdout + stderr
-sendMail(stdout + stderr)
+puts stdout2 + stderr2
+sendMail(stdout1 + stdout2 + stderr2)
 
