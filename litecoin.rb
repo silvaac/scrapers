2ea8d57ece899a0be66b870fd1d4cc95a3d2bf94
Tammer Kamel <tammer@ilukacg.com>
2014-02-03 00:16:27 -0500

    expanded litecoin.rb to more pairs

--- litecoin.rb
+++ litecoin.rb
@@ -5,20 +5,37 @@ require 'open-uri'
 # Print the last 2 days of Litecoin v USD rates
 #
 
-puts <<META
-code: LTC
-name: Litecoin (LTC) vs US Dollar (USD)
-description: Value of 1 Litecoin in US dollars.  Volume in units of LTC
-reference_url: http://www.ltc-charts.com/period-charts.php?period=6-months&resolution=day&pair=ltc-usd&market=btc-e
+def dump_numbers(short_a, short_b)
+  # period = "alltime"
+  period = "2-days"
+  doc = Nokogiri::HTML(open("http://www.cryptocoincharts.info/period-charts.php?period=#{period}&resolution=day&pair=#{short_a.downcase}-#{short_b.downcase}&market=btc-e"))
+  print"Date,Low,High,Open,Close,Volume"
+  doc.css('.span10 table tr').each do |row|
+    row = row.css('td')
+    puts row.map {|datum| datum.content.gsub(',','') }.join(", ")
+  end
+  puts;puts
+end
+
+def dump_meta( short_a, short_b, long_a, long_b )
+  puts <<META
+code: #{short_a}#{short_b}
+name: #{long_a} (#{short_a}) vs #{long_b} (#{short_b})
+description: Value of 1 #{long_a} in #{long_b}s.  Volume in units of #{short_a}.
+reference_url: http://www.cryptocoincharts.info/period-charts.php?period=alltime&resolution=day&pair=#{short_a}-#{short_b}&market=btc-e
 --
 META
+end
 
-doc = Nokogiri::HTML(open('http://www.ltc-charts.com/period-charts.php?period=2-days&resolution=day&pair=ltc-usd&market=btc-e'))
+ccys = [
+  ["LTC","BTC","Litecoin","Bitcoin"],
+  ["LTC","USD","Litecoin","US Dollar"],
+  ["BTC","USD","Bitcoin","US Dollar"],
+]
 
-puts"Date,Low,High,Open,Close,Volume"
-doc.css('.span10 table tr').each do |row|
-	row = row.css('td')
-	puts row.map {|datum| datum.content.gsub(',','') }.join(", ")
+ccys.each do |ccy|
+  dump_meta(*ccy)
+  dump_numbers(*ccy[0..1])
 end
 
 
