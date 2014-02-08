8a54fcb679cb976826f034ce8a0083b6f46bc072
Tammer Kamel <tammer@ilukacg.com>
2014-02-07 22:57:34 -0500

    steady state

--- litecoin.rb
+++ litecoin.rb
@@ -9,7 +9,7 @@ def dump_numbers(short_a, short_b)
   # period = "alltime"
   period = "2-days"
   doc = Nokogiri::HTML(open("http://www.cryptocoincharts.info/period-charts.php?period=#{period}&resolution=day&pair=#{short_a.downcase}-#{short_b.downcase}&market=btc-e"))
-  print"Date,Low,High,Open,Close,Volume"
+  # print"Date,Low,High,Open,Close,Volume"
   doc.css('.span10 table tr').each do |row|
     row = row.css('td')
     puts row.map {|datum| datum.content.gsub(',','') }.join(", ")
@@ -20,9 +20,9 @@ end
 def dump_meta( short_a, short_b, long_a, long_b )
   puts <<META
 code: #{short_a}#{short_b}
-name: #{long_a} (#{short_a}) vs #{long_b} (#{short_b}), BTC-e
-description: Value of 1 #{long_a} in #{long_b}s.  Volume in units of #{short_a}.  Data from the BTC-e exchange.
-reference_url: www.cryptocoincharts.info/period-charts.php?period=alltime&resolution=day&pair=#{short_a}-#{short_b}&market=btc-e
+# name: #{long_a} (#{short_a}) vs #{long_b} (#{short_b}), BTC-e
+# description: Value of 1 #{long_a} in #{long_b}s.  Volume in units of #{short_a}.  Data from the BTC-e exchange.
+# reference_url: www.cryptocoincharts.info/period-charts.php?period=alltime&resolution=day&pair=#{short_a}-#{short_b}&market=btc-e
 --
 META
 end
