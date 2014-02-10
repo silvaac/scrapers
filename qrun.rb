ed1525dd46330811ffc0b0852f908f76cfdaf92b
Tammer Kamel <tammer@ilukacg.com>
2014-02-10 14:41:28 -0500

    handle failure of qrun itself

--- qrun.rb
+++ qrun.rb
@@ -2,77 +2,84 @@
 require 'open3'
 require 'optparse'
 
-@QUANDL_CMD = `which quandl`.strip
-
-if @QUANDL_CMD == ''
-  @QUANDL_CMD = '/usr/local/quandl/bin'
-end
-
-@EMAIL = 'tammer@tammer.com'
-
 def sendMail (body)
   Open3.capture3("mail -s #{ARGV[0]} #{@EMAIL}", :stdin_data => body)
 end
+@EMAIL = 'tammer@tammer.com'
 
-# Parse command line
+begin
+  asfd
+  @QUANDL_CMD = `which quandl`.strip
 
-options = {}
-OptionParser.new do |opts|
-  opts.on("-v", "--verbose", "include output of scraper in the email report") do |v|
-    options[:verbose] = v
+  if @QUANDL_CMD == ''
+    @QUANDL_CMD = '/usr/local/quandl/bin'
   end
-  opts.on("-s", "--silent", "Report ONLY errors and nothing else; supercedes -v option") do |s|
-    options[:silent] = s
-  end
-  opts.on("-m", "--email [EMAIL]", "Report ONLY errors and nothing else; supercedes -v option") do |e|
-    @EMAIL = e
-  end
-end.parse!
 
-# Run the scraper:
-stdout1,stderr1,status = Open3.capture3("ruby #{ARGV[0]}")
 
-# unless stderr1 == ''
-#   puts stderr1
-#   sendMail(stderr1)
-#   exit
-# end
+  # Parse command line
 
-# puts "Scraper sent nothing to stderr. so now piping stdout to quandl upload"
+  options = {}
+  OptionParser.new do |opts|
+    opts.on("-v", "--verbose", "include output of scraper in the email report") do |v|
+      options[:verbose] = v
+    end
+    opts.on("-s", "--silent", "Report ONLY errors and nothing else; supercedes -v option") do |s|
+      options[:silent] = s
+    end
+    opts.on("-m", "--email [EMAIL]", "Report ONLY errors and nothing else; supercedes -v option") do |e|
+      @EMAIL = e
+    end
+  end.parse!
 
-stdout2, stderr2, status = Open3.capture3("#{@QUANDL_CMD} upload", :stdin_data => stdout1)
+  # Run the scraper:
+  stdout1,stderr1,status = Open3.capture3("ruby #{ARGV[0]}")
 
-final_report = "Scheduled Run of:  #{ARGV[0]}\n\n"
+  # unless stderr1 == ''
+  #   puts stderr1
+  #   sendMail(stderr1)
+  #   exit
+  # end
 
-final_report << "Scraper Errors:"
-if stderr1 == ''
-  final_report << "  none\n\n"
-else
-  final_report << "\n\n#{stderr1}\n\n"
-end
+  # puts "Scraper sent nothing to stderr. so now piping stdout to quandl upload"
 
-if options[:verbose]
-  final_report << "Scraper Output:"
-  final_report << "\n\n#{stdout1}\n\n"
-end
+  stdout2, stderr2, status = Open3.capture3("#{@QUANDL_CMD} upload", :stdin_data => stdout1)
 
-final_report << "Upload Errors:"
-if stderr2 == ''
-  final_report << "  none\n\n"
-else
-  final_report << "\n\n#{stderr2}\n\n"
-end
+  final_report = "Scheduled Run of:  #{ARGV[0]}\n\n"
 
-unless options[:silent]
-  final_report << "Upload results:"
-  if stdout2 == ''
-    final_report << "  Nothing Uploaded"
+  final_report << "Scraper Errors:"
+  if stderr1 == ''
+    final_report << "  none\n\n"
   else
-    final_report << "\n\n#{stdout2}\n\n"
+    final_report << "\n\n#{stderr1}\n\n"
   end
+
+  if options[:verbose]
+    final_report << "Scraper Output:"
+    final_report << "\n\n#{stdout1}\n\n"
+  end
+
+  final_report << "Upload Errors:"
+  if stderr2 == ''
+    final_report << "  none\n\n"
+  else
+    final_report << "\n\n#{stderr2}\n\n"
+  end
+
+  unless options[:silent]
+    final_report << "Upload results:"
+    if stdout2 == ''
+      final_report << "  Nothing Uploaded"
+    else
+      final_report << "\n\n#{stdout2}\n\n"
+    end
+  end
+
+
+  puts final_report
+  sendMail(final_report)
+
+rescue
+  sendMail("qrun itself has failed with #{$!}")
 end
 
-
-puts final_report
-sendMail(final_report)
 
